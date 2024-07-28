8cc7a7ff2a9826e3f69d831a9f68aa2ff765cbdc
Brandon Dunlap <dunlapbrandon24@gmail.com>
2024-07-28 17:27:05 -0500

    adding finalized files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+// Update the path to the HTML file
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+);
 
+// Start the server
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
